ccdbf8d16e3d339b33ed354046f1e63772dacb1c
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-05-06 20:49:37 -0400

    Created skeleton layout for landing page, lobby and game room (#1)

---
+++ rah-client/pages/play/index.js
@@ -0,0 +1,35 @@
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
+
+export default function Game() {
+  return (
+    <>
+      <div style={{ width: "100%", textAlign: "center" }}>
+        REPLACE ME WITH NAV
+      </div>
+      <Container maxWidth={false} id="gameRoom-container">
+        <Container id="gameChat-container">
+          <h1>Chat</h1>
+        </Container>
+
+        <Container id="gameRoom-inner-container">
+          <Container id="gameBoard-container">
+            <h1>Game Board</h1>
+          </Container>
+
+          <Container id="playerCards-container">
+            <Stack direction="row" spacing={2}>
+              <Card className="playerCard">player 1</Card>
+              <Card className="playerCard">player 1</Card>
+              <Card className="playerCard">player 1</Card>
+              <Card className="playerCard">player 1</Card>
+              <Card className="playerCard">player 1</Card>
+            </Stack>
+          </Container>
+        </Container>
+      </Container>
+    </>
+  );
+}
