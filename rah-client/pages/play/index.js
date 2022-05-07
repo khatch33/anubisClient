27153d8ba2377f8e2d3ef3430df669115cadc6ff
Joshua_Son <joshuason1512@gmail.com>
2022-05-07 09:44:21 -0400

    Navbar (#2)
    * added additional material ui package to make navbar work and added uuid package
    
    * added navbar component
    
    * added navbar component on each route file
    
    Co-authored-by: Joshua Son <joshuason@Joshuas-MacBook-Pro.local>

--- rah-client/pages/play/index.js
+++ rah-client/pages/play/index.js
@@ -1,31 +1,32 @@
-import Container from "@mui/material/Container";
-import Stack from "@mui/material/Stack";
-import Card from "@mui/material/Card";
-import Grid from "@mui/material/Grid";
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import Card from '@mui/material/Card';
+import Grid from '@mui/material/Grid';
+import Navbar from '../../components/Navbar/Navbar';
 
 export default function Game() {
   return (
     <>
-      <div style={{ width: "100%", textAlign: "center" }}>
-        REPLACE ME WITH NAV
+      <div style={{ width: '100%', textAlign: 'center' }}>
+        <Navbar />
       </div>
-      <Container maxWidth={false} id="gameRoom-container">
-        <Container id="gameChat-container">
+      <Container maxWidth={false} id='gameRoom-container'>
+        <Container id='gameChat-container'>
           <h1>Chat</h1>
         </Container>
 
-        <Container id="gameRoom-inner-container">
-          <Container id="gameBoard-container">
+        <Container id='gameRoom-inner-container'>
+          <Container id='gameBoard-container'>
             <h1>Game Board</h1>
           </Container>
 
-          <Container id="playerCards-container">
-            <Stack direction="row" spacing={2}>
-              <Card className="playerCard">player 1</Card>
-              <Card className="playerCard">player 1</Card>
-              <Card className="playerCard">player 1</Card>
-              <Card className="playerCard">player 1</Card>
-              <Card className="playerCard">player 1</Card>
+          <Container id='playerCards-container'>
+            <Stack direction='row' spacing={2}>
+              <Card className='playerCard'>player 1</Card>
+              <Card className='playerCard'>player 1</Card>
+              <Card className='playerCard'>player 1</Card>
+              <Card className='playerCard'>player 1</Card>
+              <Card className='playerCard'>player 1</Card>
             </Stack>
           </Container>
         </Container>
