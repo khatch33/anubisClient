1e72b34438fb573ce2041a8cf8d8481e41ba75cd
Joshua Son <joshuason@Joshuas-MacBook-Pro.local>
2022-05-07 22:47:12 -0400

    made overall stucture of lobby and game room barebone containers and css

--- rah-client/pages/lobby/index.js
+++ rah-client/pages/lobby/index.js
@@ -1,22 +1,123 @@
+import { useState } from 'react';
 import Container from '@mui/material/Container';
 import styled from 'styled-components';
 import Navbar from '../../components/Navbar/Navbar';
-
+import ListItem from '@mui/material/ListItem';
+import List from '@mui/material/List';
+import Box from '@mui/material/Box';
 export default function Lobby() {
+  const [chatBoxInput, setChatBoxInput] = useState('');
+
+  const chatBoxOnChange = (e) => {
+    setChatBoxInput(e.target.value);
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(chatBoxInput);
+    setChatBoxInput('');
+  };
+
   return (
     <>
-      <div style={{ width: '100%', textAlign: 'center' }}>
-        <Navbar />
-      </div>
-      <Container maxWidth={false} id='lobbyRoom-container'>
-        <Container maxWidth={false} id='gameDisplay-container'>
-          <h1>Game Display</h1>
-        </Container>
-
-        <Container maxWidth={false} id='lobbyChat-container'>
-          <h1>Chat</h1>
-        </Container>
+      <Navbar />
+      <Container sx={{ float: 'left', width: '20%' }}>
+        <p style={{ textAlign: 'center' }}>Online</p>
+        <List
+          style={{
+            border: '1px solid black',
+            marginTop: '1em',
+            minWidth: '100%',
+            height: '90vh',
+            overflow: 'auto',
+            padding: '7px',
+          }}>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+          <p>Some player</p>
+        </List>
       </Container>
+      <Box sx={{ display: 'inline-block', float: 'right', width: '80%' }}>
+        <Container
+          style={{ marginTop: '3.5em', overflow: 'auto' }}
+          maxWidth={false}
+          id='gameDisplay-container'>
+          <p>Game Display Remove Me</p>
+          <p>Game Display Remove Me</p>
+          <p>Game Display Remove Me</p>
+          <p>Game Display Remove Me</p>
+          <p>Game Display Remove Me</p>
+          <p>Game Display Remove Me</p>
+          <p>Game Display Remove Me</p>
+          <p>Game Display Remove Me</p>
+          <p>Game Display Remove Me</p>
+          <p>Game Display Remove Me</p>
+          <p>Game Display Remove Me</p>
+          <p>Game Display Remove Me</p>
+          <p>Game Display Remove Me</p>
+        </Container>
+        <Container
+          style={{ marginTop: '1em', marginBottom: '1em' }}
+          maxWidth={false}
+          id='lobbyChat-container'>
+          <p>Chat box</p>
+          <List
+            style={{
+              border: '1px solid black',
+              marginTop: '1em',
+              minWidth: '100%',
+              minHeight: '60%',
+              overflow: 'auto',
+              padding: '7px',
+            }}>
+            INSIDE HERE IS THE CHAT... INSIDE HERE IS THE CHAT... INSIDE HERE IS THE CHAT... INSIDE
+            HERE IS THE CHAT... INSIDE HERE IS THE CHAT... INSIDE HERE IS THE CHAT...
+          </List>
+          <Form onSubmit={onFormSubmit}>
+            <input
+              style={{
+                marginTop: '1em',
+                height: '35px',
+                borderRadius: '7px',
+                width: '100%',
+              }}
+              type='text'
+              placeholder='type to players'
+              onChange={chatBoxOnChange}
+              value={chatBoxInput}
+            />
+            <input hidden type='submit' />
+          </Form>
+        </Container>
+      </Box>
     </>
   );
 }
+
+const Form = styled.form`
+  margin-top: 1em;
+`;
--- rah-client/pages/play/index.js
+++ rah-client/pages/play/index.js
@@ -3,34 +3,74 @@ import Stack from '@mui/material/Stack';
 import Card from '@mui/material/Card';
 import Grid from '@mui/material/Grid';
 import Navbar from '../../components/Navbar/Navbar';
-
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
 export default function Game() {
   return (
     <>
-      <div style={{ width: '100%', textAlign: 'center' }}>
-        <Navbar />
-      </div>
-      <Container maxWidth={false} id='gameRoom-container'>
-        <Container id='gameChat-container'>
-          <h1>Chat</h1>
-        </Container>
-
-        <Container id='gameRoom-inner-container'>
-          <Container id='gameBoard-container'>
-            <h1>Game Board</h1>
-          </Container>
-
-          <Container id='playerCards-container'>
-            <Stack direction='row' spacing={2}>
-              <Card className='playerCard'>player 1</Card>
-              <Card className='playerCard'>player 1</Card>
-              <Card className='playerCard'>player 1</Card>
-              <Card className='playerCard'>player 1</Card>
-              <Card className='playerCard'>player 1</Card>
-            </Stack>
-          </Container>
-        </Container>
+      <Navbar />
+      <Container sx={{ float: 'left', width: '20%' }}>
+        <p style={{ textAlign: 'center' }}>Chat</p>
+        <List
+          style={{
+            border: '1px solid black',
+            marginTop: '1em',
+            minWidth: '100%',
+            height: '93.25vh',
+            overflow: 'auto',
+            padding: '7px',
+          }}>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+          <p>I'm some random message</p>
+        </List>
       </Container>
+      <Box sx={{ display: 'inline-block', float: 'right', width: '80%' }}>
+        <Container style={{ marginTop: '3.5em' }} maxWidth={false} id='gameBoard-container'>
+          <p>Game Board Remove me</p>
+          <p>Game Board Remove me</p>
+          <p>Game Board Remove me</p>
+          <p>Game Board Remove me</p>
+          <p>Game Board Remove me</p>
+          <p>Game Board Remove me</p>
+          <p>Game Board Remove me</p>
+          <p>Game Board Remove me</p>
+          <p>Game Board Remove me</p>
+          <p>Game Board Remove me</p>
+          <p>Game Board Remove me</p>
+          <p>Game Board Remove me</p>
+          <p>Game Board Remove me</p>
+        </Container>
+        <Container
+          maxWidth={false}
+          style={{ marginTop: '1em', marginBottom: '1em' }}
+          id='playerCards-container'>
+          <Stack direction='row' spacing={2}>
+            <Card className='playerCard'>player 1</Card>
+            <Card className='playerCard'>player 1</Card>
+            <Card className='playerCard'>player 1</Card>
+            <Card className='playerCard'>player 1</Card>
+            <Card className='playerCard'>player 1</Card>
+          </Stack>
+        </Container>
+      </Box>
     </>
   );
 }
