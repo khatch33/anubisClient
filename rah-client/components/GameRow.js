40f1bf0d59bc3b6111b44c93d23b3a36070c6fb0
Kyle Hatch <95315047+khatch33@users.noreply.github.com>
2022-05-09 12:49:16 -0700

    Lobby games (#8)
    * lobby socket
    
    * changes
    
    * socket and gamerows

---
+++ rah-client/components/GameRow.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import Grid from '@mui/material/Grid'
+export default function GameRow (props) {
+  let game = props.game
+  let creator = game.owner
+  let gameName = game.gameName
+  //let players = game.players.length.toString() + '/' + game.player.allowed
+
+  return (
+    <Grid container columnSpacing={2}>
+        <Grid item>
+          <p>{creator}</p>
+        </Grid>
+        <Grid item><p>{gameName}</p>
+        </Grid>
+        <Grid item><p>players</p>
+        </Grid>
+    </Grid>
+  )
+}
--- rah-client/pages/lobby/index.js
+++ rah-client/pages/lobby/index.js
@@ -1,13 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import Container from '@mui/material/Container';
 import styled from 'styled-components';
 import Navbar from '../../components/Navbar/Navbar';
 import ListItem from '@mui/material/ListItem';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import List from '@mui/material/List';
 import ChatForm from '../../components/lobby-chatForm/ChatForm';
+import GameRow from '../../components/GameRow.js'
 import LobbyDisplay from '../../components/lobby-gameDisplay/LobbyDisplay';
+import {activeUsers} from '../_sampleData/activeUsers.js'
+//import SocketContext from '../../socket/socket.js';
+import {socket} from '../../socket/socket.js'
 export default function Lobby() {
+  //const socket = useContext(SocketContext)
+  useEffect(() => {
+    socket.on('connect', (() => {
+      socket.emit('gg', 'sting')
+    }))
+
+
+  })
+  const arr = [1,2,3,4,5,6,7,8,9,10]
+  const game = {
+      owner: 'creator',
+      gameName: "somKindaGameName",
+      playerAllowed: 20,
+      players: activeUsers,
+      winner: 'none',
+      started: false,
+      createdAt: 'creationTimeString'
+    }
   return (
     <>
       <Navbar />
@@ -17,6 +40,12 @@ export default function Lobby() {
       </Container>
       <Box sx={{ display: 'inline-block', float: 'right', width: '80%' }}>
         <Container maxWidth={false} id='gameDisplay-container'>
+            {arr.map((num) => {
+              return (
+                <GameRow game={game}/>
+              )
+            })}
+          {/* <p>Game Display Remove Me</p>
           <p>Game Display Remove Me</p>
           <p>Game Display Remove Me</p>
           <p>Game Display Remove Me</p>
@@ -28,8 +57,7 @@ export default function Lobby() {
           <p>Game Display Remove Me</p>
           <p>Game Display Remove Me</p>
           <p>Game Display Remove Me</p>
-          <p>Game Display Remove Me</p>
-          <p>Game Display Remove Me</p>
+          <p>Game Display Remove Me</p> */}
         </Container>
         <Container maxWidth={false} id='lobbyChat-container'>
           <p>Chat box</p>
