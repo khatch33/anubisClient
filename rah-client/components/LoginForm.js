8271b1580a6c1d58ad71b299be3c3ae2918b9049
Joshua Son <joshuason@Joshuas-MacBook-Pro.local>
2022-05-09 13:56:18 -0400

    added login and signup forms when clicked on

--- rah-client/components/LoginForm.js
+++ rah-client/components/LoginForm.js
@@ -4,28 +4,51 @@ import axios from 'axios';
 import TextField from '@mui/material/TextField';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  borderRadius: '7px',
+};
 
 export default function LoginForm() {
-
-  const { register, handleSubmit, formState: { errors }} = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = (data) => {
     axios({
       method: 'post',
       url: '/',
-      data: {username: username, password: password, jwtToken: ''}
+      data: { username: username, password: password, jwtToken: '' },
     })
       .then((res) => res)
       .catch((err) => err);
-  }
+  };
 
   return (
-      <form onSubmit={handleSubmit(onSubmit)} maxWidth="sm" id="login-container">
-        <TextField placeholder="Username" {...register("username", { required: true })} />
-        {errors.username && <span style={{color: 'red'}}>Enter a valid username</span>}
-        <TextField placeholder="Password" {...register("password", { required: true })} />
-        {errors.password && <span style={{color: 'red'}}>Enter a valid password</span>}
-        <Button type="submit" sx={{ color: '#413C39' }} size="medium">Login</Button>
-      </form>
-  )
+    <>
+      <Box sx={style}>
+        <form onSubmit={handleSubmit(onSubmit)} maxWidth='sm' id='login-container'>
+          <TextField placeholder='Username' {...register('username', { required: true })} />
+          {errors.username && <span style={{ color: 'red' }}>Enter a valid username</span>}
+          <TextField placeholder='Password' {...register('password', { required: true })} />
+          {errors.password && <span style={{ color: 'red' }}>Enter a valid password</span>}
+          <Button type='submit' sx={{ color: '#413C39' }} size='medium'>
+            Login
+          </Button>
+        </form>
+      </Box>
+    </>
+  );
 }
