ded87755bcea64852064ee7826a6c292432f9cc1
Joshua Son <joshuason@Joshuas-MacBook-Pro.local>
2022-05-09 19:06:55 -0400

    added tab, so we can toggle between lobby and creating a game

--- rah-client/pages/index.js
+++ rah-client/pages/index.js
@@ -1,15 +1,13 @@
-
 import LandingPage from '../components/LandingPage';
-import Container from "@mui/material/Container";
+import Container from '@mui/material/Container';
 import Navbar from '../components/Navbar/Navbar';
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import ActiveUsersList from '../components/ActiveUsersList';
 import axios from 'axios';
 
 const basePath = 'http://localhost:4030/blueocean/api/v1';
 
 export default function Home() {
-
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
 
@@ -19,10 +17,11 @@ export default function Home() {
     let mounted = true;
     if (mounted) {
       setIsLoaded(true);
-      axios.get(`${basePath}/users`)
+      axios
+        .get(`${basePath}/users`)
         .then((res) => setActiveList(res.data.users))
         .catch((err) => {
-          setIsloaded(true);
+          setIsLoaded(true);
           setError(err);
         });
     }
@@ -36,38 +35,33 @@ export default function Home() {
     return (
       <>
         <Navbar />
-        <Container maxWidth={false} id="app-container">
+        <Container maxWidth={false} id='app-container'>
           <ActiveUsersList users={activeList} />
 
           <Container maxWidth={false}>
-            <Container maxWidth={false} id="video-container">
+            <Container maxWidth={false} id='video-container'>
               <iframe
-                width="560"
-                height="315"
-                src="https://www.youtube.com/embed/OysJ4nL4jS8"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
+                width='560'
+                height='315'
+                src='https://www.youtube.com/embed/OysJ4nL4jS8'
+                title='YouTube video player'
+                frameBorder='0'
+                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                allowFullScreen></iframe>
             </Container>
 
-            <Container maxWidth={false} id="instructions-container">
+            <Container maxWidth={false} id='instructions-container'>
               <h1>Instructions</h1>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-                mollitia, molestiae quas vel sint commodi repudiandae
-                consequuntur voluptatum laborum numquam blanditiis harum
-                quisquam eius sed odit fugiat iusto fuga praesentium optio,
-                eaque rerum! Provident similique accusantium nemo autem.
-                Veritatis obcaecati tenetur iure eius earum ut molestias
-                architecto voluptate aliquam nihil, eveniet aliquid culpa
-                officia aut! Impedit sit sunt quaerat, odit, tenetur error,
-                harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia.
-                Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-                sapiente officiis modi at sunt excepturi expedita sint? Sed
-                quibusdam recusandae alias error harum maxime adipisci amet
-                laborum.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae
+                quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis
+                harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum!
+                Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius
+                earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia
+                aut! Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis
+                quas aliquid. Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa laudantium
+                molestias eos sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+                recusandae alias error harum maxime adipisci amet laborum.
               </p>
             </Container>
           </Container>
@@ -76,4 +70,3 @@ export default function Home() {
     );
   }
 }
-
--- rah-client/pages/lobby/index.js
+++ rah-client/pages/lobby/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Container from '@mui/material/Container';
 import styled from 'styled-components';
 import Navbar from '../../components/Navbar/Navbar';
@@ -11,9 +11,20 @@ import GameRow from '../../components/GameRow.js';
 import LobbyDisplay from '../../components/lobby-gameDisplay/LobbyDisplay';
 import LobbyChatRoom from '../../components/lobby-chatRoom/LobbyChatRoom';
 import { activeUsers } from '../_sampleData/activeUsers.js';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
 //import SocketContext from '../../socket/socket.js';
 import { socket } from '../../socket/socket.js';
+import CreateGame from '../../components/lobby-createGame/CreateGame';
 export default function Lobby() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    console.log(newValue);
+    setValue(newValue);
+  };
   //const socket = useContext(SocketContext)
   useEffect(() => {
     socket.on('connect', () => {
@@ -37,25 +48,21 @@ export default function Lobby() {
         <OnlineTitle>Online</OnlineTitle>
         <LobbyDisplay />
       </Container>
+      <TabsContainer value={value} onChange={handleChange} aria-label='basic tabs example'>
+        <Tab label='Lobby' />
+        <Tab label='Create' />
+      </TabsContainer>
       <Box sx={{ display: 'inline-block', float: 'right', width: '80%' }}>
         <Container maxWidth={false} id='gameDisplay-container'>
-          {arr.map((num) => {
-            return <GameRow game={game} />;
-          })}
-          {/* <p>Game Display Remove Me</p>
-          <p>Game Display Remove Me</p>
-          <p>Game Display Remove Me</p>
-          <p>Game Display Remove Me</p>
-          <p>Game Display Remove Me</p>
-          <p>Game Display Remove Me</p>
-          <p>Game Display Remove Me</p>
-          <p>Game Display Remove Me</p>
-          <p>Game Display Remove Me</p>
-          <p>Game Display Remove Me</p>
-          <p>Game Display Remove Me</p>
-          <p>Game Display Remove Me</p>
-          <p>Game Display Remove Me</p> */}
+          {value === 0 ? (
+            arr.map((num) => {
+              return <GameRow game={game} />;
+            })
+          ) : (
+            <CreateGame />
+          )}
         </Container>
+
         <Container maxWidth={false} id='lobbyChat-container'>
           <p>Chat box</p>
           <LobbyChatRoom />
@@ -68,4 +75,10 @@ export default function Lobby() {
 
 const OnlineTitle = styled.p`
   text-align: center;
+`;
+
+const TabsContainer = styled(Tabs)`
+  position: absolute;
+  margin-left: 22%;
+  margin-top: 10px;
 `;
