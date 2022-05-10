6841765374fde5d28209eb9b7c062643cd3c39a4
Joshua_Son <joshuason1512@gmail.com>
2022-05-09 23:17:18 -0400

    Game logic (#10)
    * added additional material ui package to make navbar work and added uuid package
    
    * added navbar component
    
    * added navbar component on each route file
    
    * added friend chat modal
    
    * added login and menu
    
    * added sign up and login modals
    
    * added skeleton component to friends list, so once we load, skeletons will show
    
    * made overall stucture of lobby and game room barebone containers and css
    
    * added line here for styled component rendering
    
    * added file here to render styled components correctly. DO NOT REMOVE!
    
    * removed all inline styles and split components on based on if state will change. can continue to split later on if needed
    
    * added login and signup forms when clicked on
    
    * made my chat box component on its own
    
    * added tab, so we can toggle between lobby and creating a game
    
    Co-authored-by: Joshua Son <joshuason@Joshuas-MacBook-Pro.local>

--- rah-client/components/GameRow.js
+++ rah-client/components/GameRow.js
@@ -1,20 +1,22 @@
 import React, { useState } from 'react';
-import Grid from '@mui/material/Grid'
-export default function GameRow (props) {
-  let game = props.game
-  let creator = game.owner
-  let gameName = game.gameName
+import Grid from '@mui/material/Grid';
+export default function GameRow(props) {
+  let game = props.game;
+  let creator = game.owner;
+  let gameName = game.gameName;
   //let players = game.players.length.toString() + '/' + game.player.allowed
 
   return (
-    <Grid container columnSpacing={2}>
-        <Grid item>
-          <p>{creator}</p>
-        </Grid>
-        <Grid item><p>{gameName}</p>
-        </Grid>
-        <Grid item><p>players</p>
-        </Grid>
+    <Grid container columnSpacing={25}>
+      <Grid item>
+        <p>{creator}</p>
+      </Grid>
+      <Grid item>
+        <p>{gameName}</p>
+      </Grid>
+      <Grid item>
+        <p>players</p>
+      </Grid>
     </Grid>
-  )
+  );
 }
---
+++ rah-client/components/lobby-createGame/CreateGame.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Button from '@mui/material/Button';
+export default function CreateGame() {
+  const [playerName, setPlayerName] = useState('');
+  const [players, setPlayers] = useState(0);
+  const [gameName, setGameName] = useState('');
+  const nameChange = (e) => {
+    setPlayerName(e.target.value);
+  };
+
+  const numPlayers = (e) => {
+    setPlayers(e.target.value);
+  };
+
+  const gameNameHandler = (e) => {
+    setGameName(e.target.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(playerName, players, gameName);
+  };
+
+  return (
+    <Form onSubmit={onSubmitHandler}>
+      <label>What is your name?</label>
+      <FormInput onChange={nameChange} type='text' />
+      <label>How many people are playing?</label>
+      <FormInput onChange={numPlayers} type='number' />
+      <label>What will the game name be?</label>
+      <FormInput onChange={gameNameHandler} type='text' />
+      <StyledButton size='small' type='submit' variant='outlined'>
+        Create
+      </StyledButton>
+    </Form>
+  );
+}
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  text-align: center;
+  border: 1px solid black;
+  margin-left: 10%;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 300px;
+  height: 250px;
+  padding: 10px;
+`;
+
+const FormInput = styled.input`
+  margin: auto;
+  width: 50%;
+`;
+
+const StyledButton = styled(Button)`
+  width: 30%;
+  margin: auto;
+`;
