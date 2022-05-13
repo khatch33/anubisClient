68d23c3a054f7c43dafd5de2a3edba80e17bbea7
Joshua_Son <joshuason1512@gmail.com>
2022-05-13 09:42:02 -0400

    created sprite like images on the board, changed board height and width, fixed tabs, and fixed axios.get for each game (#31)
    Co-authored-by: Joshua Son <joshuason@Joshuas-MacBook-Pro.local>

--- rah-client/components/GameRoom/GameBoard.js
+++ rah-client/components/GameRoom/GameBoard.js
@@ -1,4 +1,3 @@
-
 import Container from '@mui/material/Container';
 import styled from 'styled-components';
 import Image from 'next/Image';
@@ -6,45 +5,62 @@ import BoardImg from '../../public/gameboard.jpg';
 import Card from '@mui/material/Card';
 
 export default function GameBoard(props) {
-
-
   return (
     <OuterContainer maxWidth={false} disableGutters={true}>
-
       <Banner>
         {props.announcement ? (
-          <Announcement behavior="scroll" direction="left">{props.announcement}</Announcement>
+          <Announcement behavior='scroll' direction='left'>
+            {props.announcement}
+          </Announcement>
         ) : null}
       </Banner>
 
-      <div style={{display: 'flex', flexDirection: 'row'}}>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <ImgContainer maxWidth={false} disableGutters={true} id='gameboard-container'>
+          <Img src={BoardImg} alt='' height='500' width='500' />
+          <Person left={'0%'} top={'40%'}></Person>
+          <Person left={'23.5%'} top={'40%'}></Person>
+          <Person left={'47%'} top={'40%'}></Person>
+          <Person left={'70.5%'} top={'40%'}></Person>
+          <Person left={'94%'} top={'40%'}></Person>
+          <Person left={'94%'} top={'40%'}></Person>
+          <Person left={'94%'} top={'40%'}></Person>
+        </ImgContainer>
 
-      <ImgContainer maxWidth={false} disableGutters={true} id="gameboard-container">
-        <Img src={BoardImg} alt="" height="370" width="500"/>
-      </ImgContainer>
+        <InfoContainer maxWidth={false} disableGutters={true}>
+          <Header>GAME INFO</Header>
+          <div>
+            <StartButton onClick={() => props.setOpen(!props.open)}>INSTRUCTIONS</StartButton>
+          </div>
 
-      <InfoContainer maxWidth={false} disableGutters={true}>
-        <Header>GAME INFO</Header>
-        <div><StartButton onClick={() => props.setOpen(!props.open)}>INSTRUCTIONS</StartButton></div>
-
-        <div>Phase: {props.game.phase}</div>
-        <div>Your Role: {props.info.role}</div>
+          <div>Phase: {props.game.phase}</div>
+          {/* <div>Your Role: {props.info.role}</div>
         <div>Players Remaining: {props.info.playersLeft}</div>
         <div>Anubis Remaining: {props.info.wolfsLeft}</div>
         <div>Doctors Remaining: {props.info.doctorsLeft}</div>
-        <div>Seers Remaining: {props.info.seersLeft}</div>
+        <div>Seers Remaining: {props.info.seersLeft}</div> */}
 
-        {props.game ? (
-          props.game.owner === props.playerId ? (
-            <div><StartButton onClick={() => props.startGame()}>START GAME</StartButton></div>
-          ) : null
-        ) : null}
-
-      </InfoContainer>
+          {props.game ? (
+            props.game.owner === props.playerId ? (
+              <div>
+                <StartButton onClick={() => props.startGame()}>START GAME</StartButton>
+              </div>
+            ) : null
+          ) : null}
+        </InfoContainer>
       </div>
     </OuterContainer>
-  )
+  );
 }
+
+const Person = styled.span`
+  position: absolute;
+  border: 15px solid red;
+  height: 50px;
+  border-radius: 15px;
+  left: ${(props) => props.left};
+  top: ${(props) => props.top};
+`;
 
 const OuterContainer = styled(Container)`
   border-radius: 5px;
@@ -52,7 +68,7 @@ const OuterContainer = styled(Container)`
   flex-direction: column;
   /* width: 95%; */
   max-width: 800px;
-  background-color: #F1F7ED;
+  background-color: #f1f7ed;
   border-radius: 5px;
   height: 350px;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
@@ -64,9 +80,11 @@ const OuterContainer = styled(Container)`
 `;
 
 const ImgContainer = styled(Container)`
+  position: relative;
+  border: 1px solid black;
   border-radius: 5px;
   width: 500px;
-  height: 100%;
+  height: 500px;
   margin-left: 0;
 `;
 
@@ -84,12 +102,12 @@ const InfoContainer = styled(Container)`
 `;
 
 const Header = styled(Container)`
-  background-color: #9A8249;
+  background-color: #9a8249;
   border-radius: 5px 5px 0 0;
   height: 35px;
   font-family: 'Roboto';
   text-align: center;
-  color: #F1F7ED;
+  color: #f1f7ed;
   font-weight: 700;
   letter-spacing: 0.2rem;
   line-height: 1.6;
@@ -126,5 +144,5 @@ const Announcement = styled.marquee`
   line-height: 1.6em;
   font-family: monospace;
   font-weight: 700;
-  color: #9A8249;
+  color: #9a8249;
 `;
