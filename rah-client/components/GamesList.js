50ade704628aede0ff710af136c5fde88f8c7008
Joshua_Son <joshuason1512@gmail.com>
2022-05-13 09:42:02 -0400

    created sprite like images on the board, changed board height and width, fixed tabs, and fixed axios.get for each game (#31)
    Co-authored-by: Joshua Son <joshuason@Joshuas-MacBook-Pro.local>

--- rah-client/components/GamesList.js
+++ rah-client/components/GamesList.js
@@ -1,59 +1,58 @@
-import React, { useState } from "react";
-import { DataGrid } from "@mui/x-data-grid";
-import { useRecoilValue } from "recoil";
-import { userState } from "../_states/tokenState";
-import { useRouter } from "next/router";
-import Container from "@mui/material/Container";
-import styled from "styled-components";
+import React, { useState } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { useRecoilValue } from 'recoil';
+import { userState } from '../_states/tokenState';
+import { useRouter } from 'next/router';
+import Container from '@mui/material/Container';
+import styled from 'styled-components';
 
-
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import CreateGame from './Lobby/CreateGame';
 
 const columns = [
   {
-    field: "creatorName",
-    headerName: "CREATOR",
-    type: "string",
+    field: 'creatorName',
+    headerName: 'CREATOR',
+    type: 'string',
     width: 170,
     editable: false,
-    headerAlign: "center",
+    headerAlign: 'center',
   },
   {
-    field: "gameName",
-    headerName: "GAME NAME",
-    type: "string",
+    field: 'gameName',
+    headerName: 'GAME NAME',
+    type: 'string',
     width: 170,
     editable: false,
-    headerAlign: "center",
+    headerAlign: 'center',
   },
   {
-    field: "playersNum",
-    headerName: "PLAYERS",
-    type: "number",
+    field: 'playersNum',
+    headerName: 'PLAYERS',
+    type: 'number',
     width: 170,
     editable: false,
-    headerAlign: "center",
+    headerAlign: 'center',
   },
   {
-    field: "createdAt",
-    headerName: "CREATED AT",
-    type: "string",
+    field: 'createdAt',
+    headerName: 'CREATED AT',
+    type: 'string',
     width: 170,
     editable: false,
-    headerAlign: "center",
+    headerAlign: 'center',
   },
 ];
-//{ id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
 
 export default function GamesList(props) {
+  const [value, setValue] = useState(0);
   const router = useRouter();
   const user = useRecoilValue(userState);
   const [showForm, setShowForm] = useState(false);
-  var gameIdd;
+
   const rows = props.games.map((game) => {
-    let players =
-      "players: " + game.players.length.toString() + "/" + game.playerAllowed;
+    let players = 'players: ' + game.players.length.toString() + '/' + game.playerAllowed;
     return {
       id: game._id,
       creatorName: game.ownerName,
@@ -63,47 +62,41 @@ export default function GamesList(props) {
     };
   });
 
-  const yesClick = () => {
-    router.push(`/play/${gameId}/${user.userId}`);
-  };
-
-  const noClick = () => {
-    setShowForm(false);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
   };
 
   const onRowClick = (event) => {
-    gameIdd = event.id;
-    console.log(event.id);
     router.push(`/play/${event.id}/${user.userId}`);
-    //setShowForm(true)
+  };
+
+  const gameCreated = () => {
+    setValue(0);
   };
 
   return (
-    <Container style={{ height: 350, minWidth: "500px", maxWidth: "675px" }}>
+    <Container style={{ height: 350, minWidth: '500px', maxWidth: '675px' }}>
       <TabsContainer
         TabIndicatorProps={{
-          style: { backgroundColor: "#9A8249", color: "#F1F7ED" },
+          style: { backgroundColor: '#9A8249', color: '#F1F7ED' },
         }}
-        value={props.value}
-        onChange={props.handleChange}
-        aria-label="tabs"
-      >
-        <StyledTab label="Lobby" />
-        <StyledTab label="Create" />
+        value={value}
+        onChange={handleChange}
+        aria-label='tabs'>
+        <StyledTab label='Lobby' />
+        <StyledTab label='Create' />
       </TabsContainer>
 
-      <DataGrid
-        sx={{ border: "none" }}
-        density="compact"
-        rows={rows}
-        columns={columns}
-        onRowClick={onRowClick}
-      />
-      {showForm && (
-        <>
-          <button onClick={yesClick}>Go!</button>
-          <button onClick={noClick}>No!</button>
-        </>
+      {value === 0 ? (
+        <DataGrid
+          sx={{ border: 'none' }}
+          density='compact'
+          rows={rows}
+          columns={columns}
+          onRowClick={onRowClick}
+        />
+      ) : (
+        <CreateGame handleChange={gameCreated} />
       )}
     </Container>
   );
